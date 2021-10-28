5457d5814080c874f8e7c9eec3b1557df10313c6
Roberto Vieira Carlos <79486045+beto-desenv@users.noreply.github.com>
2021-10-27 22:29:07 -0300

    Update

---
+++ EventosDePagina.cs
@@ -0,0 +1,61 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+
+namespace GeradorDeRelatoriosEmPDF
+{
+    class EventosDePagina : PdfPageEventHelper
+    {
+        private PdfContentByte wdc;
+
+        private BaseFont fonteBaseRodape { get; set; }
+        private iTextSharp.text.Font fonteRodape { get; set; }
+        public int totalPaginas { get; set; }
+        public EventosDePagina(int totalPaginas)
+        {
+            fonteBaseRodape = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, false);
+            fonteRodape = new iTextSharp.text.Font(fonteBaseRodape, 8f, iTextSharp.text.Font.NORMAL, BaseColor.Black);
+            this.totalPaginas = totalPaginas;
+        }
+
+        public override void OnOpenDocument(PdfWriter writer, Document document)
+        {
+            base.OnOpenDocument(writer, document);
+            this.wdc = writer.DirectContent;
+        }
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            base.OnEndPage(writer, document);
+            AdicionarMomentoGeracaoRelatorio(writer, document);
+            AdicionarNumerosDasPaginas(writer, document);
+        }
+
+        private void AdicionarMomentoGeracaoRelatorio(PdfWriter writer, Document document)
+        {
+            var textoMomentoGeracao = $"Gerado em {DateTime.Now.ToShortDateString()} às " + $"{DateTime.Now.ToShortDateString()}";
+
+            wdc.BeginText(); // aqui eu inicio um texto
+            wdc.SetFontAndSize(fonteRodape.BaseFont, fonteRodape.Size); // aqui determino a familia da fonte e o tamanho a ser usado
+            wdc.SetTextMatrix(document.LeftMargin, document.BottomMargin * 0.75f); // aqui eu defino a posição em que ocorrera a escrita
+            wdc.ShowText(textoMomentoGeracao); // mando mostrar o texto criado
+            wdc.EndText(); // e finalizo o conteudo.
+        }
+
+        private void AdicionarNumerosDasPaginas(PdfWriter writer, Document document)
+        {
+            int paginaAtual = writer.PageNumber;
+            var textoPaginacao = $"Página {paginaAtual} de {totalPaginas}";
+
+            float larguraTextoPaginacao = fonteBaseRodape.GetWidthPoint(textoPaginacao, fonteRodape.Size);
+
+            var tamanhoPagina = document.PageSize;
+
+            wdc.BeginText(); // aqui eu inicio um texto
+            wdc.SetFontAndSize(fonteRodape.BaseFont, fonteRodape.Size); // aqui determino a familia da fonte e o tamanho a ser usado
+            wdc.SetTextMatrix(tamanhoPagina.Width - document.RightMargin - larguraTextoPaginacao, document.BottomMargin * 0.75f); // aqui eu defino a posição em que ocorrera a escrita
+            wdc.ShowText(textoPaginacao); // mando mostrar o texto criado
+            wdc.EndText(); // e finalizo o conteudo.
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -36,12 +36,19 @@ namespace GeradorDeRelatoriosEmPDF
             var pessoasSelecionadas = pessoas.Take(qtdePessoas).ToList();
             if (pessoasSelecionadas.Count > 0)
             {
+                //Calculo da quantidade total de páginas
+                int totalPaginas = 1;
+                int totalLinhas = pessoasSelecionadas.Count;
+                if (totalLinhas > 24)
+                    totalPaginas += (int)Math.Ceiling((totalLinhas - 24) / 29F);
+
                 //configuração do documento PDF
                 var pxPorMm = 72 / 25.2F; // Relação entre a quantidade de pixels(px = 72) por milimetro(mm = 25.5F)
                 var pdf = new Document(PageSize.A4, 15 * pxPorMm, 15 * pxPorMm, 15 * pxPorMm, 20 * pxPorMm);
                 var nomeArquivo = $"pessoas.{DateTime.Now.ToString("yyyy.MM.dd.HH.mm.ss")}.pdf";
                 var arquivo = new FileStream(nomeArquivo, FileMode.Create);
                 var writer = PdfWriter.GetInstance(pdf, arquivo);
+                writer.PageEvent = new EventosDePagina(totalPaginas);
                 pdf.Open();
                                
                 //adição do título
@@ -66,6 +73,20 @@ namespace GeradorDeRelatoriosEmPDF
                     writer.DirectContent.AddImage(logo, false);
                 }
 
+                //adição de um link
+                var fonteLink = new iTextSharp.text.Font(fonteBase, 9.9F, Font.NORMAL, BaseColor.Blue);
+                var link = new Chunk("GitHub do Roberto Vieira Carlos", fonteLink);
+                link.SetAnchor("https://github.com/beto-desenv");
+                var larguraTextoLink = fonteBase.GetWidthPoint(link.Content, fonteLink.Size);
+                var caixaTexto = new ColumnText(writer.DirectContent);
+                caixaTexto.AddElement(link);
+                caixaTexto.SetSimpleColumn(
+                    pdf.PageSize.Width - pdf.RightMargin - larguraTextoLink,
+                    pdf.PageSize.Height - pdf.TopMargin - (30 * pxPorMm),
+                    pdf.PageSize.Width - pdf.RightMargin,
+                    pdf.PageSize.Height - pdf.TopMargin - (18 * pxPorMm));
+                caixaTexto.Go();
+
                 //adição da tabela de dados
                 var tabela = new PdfPTable(5);
                 float[] larguraColunas = { 0.6f, 2f, 1.5f, 1f, 1f };
@@ -86,7 +107,11 @@ namespace GeradorDeRelatoriosEmPDF
                     CriarCelulaTexto(tabela, p.Nome + " " + p.Sobrenome);
                     CriarCelulaTexto(tabela, p.Profissao.Nome, PdfPCell.ALIGN_CENTER);
                     CriarCelulaTexto(tabela, p.Salario.ToString("C2"), PdfPCell.ALIGN_RIGHT);
-                    CriarCelulaTexto(tabela, p.Empregado ? "Sim" : "Não", PdfPCell.ALIGN_CENTER);
+                    //CriarCelulaTexto(tabela, p.Empregado ? "Sim" : "Não", PdfPCell.ALIGN_CENTER);
+                    var caminhoImagemCelula = p.Empregado ?
+                        "img\\emoji_feliz.png" : "img\\emoji_triste.png";
+                    caminhoImagemCelula = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, caminhoImagemCelula);
+                    CriarCelulaImagem(tabela, caminhoImagemCelula, 20, 20);
                 }
 
                 pdf.Add(tabela);
@@ -127,6 +152,11 @@ namespace GeradorDeRelatoriosEmPDF
                 estilo = iTextSharp.text.Font.ITALIC;
             }
             var fonteCelula = new iTextSharp.text.Font(fonteBase, tamanhoFonte, estilo, BaseColor.Black);
+
+            var bgColor = iTextSharp.text.BaseColor.White;
+            if (tabela.Rows.Count % 2 == 1)
+                bgColor = new BaseColor(0.95F, 0.95F, 0.95F);
+
             var celula = new PdfPCell(new Phrase(texto, fonteCelula));
             celula.HorizontalAlignment = alinhamentoHorz;
             celula.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
@@ -134,7 +164,31 @@ namespace GeradorDeRelatoriosEmPDF
             celula.BorderWidthBottom = 1;
             celula.FixedHeight = alturaCelula;
             celula.PaddingBottom = 5;
+            celula.BackgroundColor = bgColor;
             tabela.AddCell(celula);
+        }
+
+        static void CriarCelulaImagem(PdfPTable tabela, string caminhoImagem, int larguraImagem, int alturaImagem, int alturaCelula = 25)
+        {
+            var bgColor = iTextSharp.text.BaseColor.White;
+            if (tabela.Rows.Count % 2 == 1)
+                bgColor = new BaseColor(0.95F, 0.95F, 0.95F);
+
+            if (File.Exists(caminhoImagem))
+            {
+                iTextSharp.text.Image imagem = 
+                    iTextSharp.text.Image.GetInstance(caminhoImagem);
+                imagem.ScaleToFit(larguraImagem, alturaImagem);
+
+                var celula = new PdfPCell(imagem);
+                celula.HorizontalAlignment = PdfPCell.ALIGN_CENTER;
+                celula.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+                celula.Border = 0;
+                celula.BorderWidthBottom = 1;
+                celula.FixedHeight = alturaCelula;
+                celula.BackgroundColor = bgColor;
+                tabela.AddCell(celula);
+            }            
         }
     }
 }
